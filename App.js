36905708b9e38ceb39fa2f7c7c917db7f6a9f017
alexandrotheo <alexandrotheo68@gmail.com>
2023-04-03 08:48:31 +0700

    tugas_aschrous_reactjs

---
+++ App.js
@@ -0,0 +1,49 @@
+// const myFunction = () => {
+//   return new Promise ((resolve, reject) => {
+//     resolve("Hallo, Ini kondisi true/berhasil")
+//   })
+// }
+// myFunction()
+// .then((result) => {
+//   console.log(result)
+// })
+
+// const myFunction = () => {
+//   return new Promise ((resolve, reject) => {
+//     reject("Hallo, Ini kondisi false/gagal")
+//   })
+// }
+// myFunction()
+// .catch((reject) => {
+//   console.log(reject)
+// })
+
+// const myFunctionPromise = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve("Halo, Ini kondisi true/berhasil")
+//     reject("Halo, Ini kondisi false/gagal")
+//   })
+// }
+
+// myFunctionPromise()
+// .catch((error) => {
+//   console.log(error)
+// })
+
+// const myFunctionPromise = (param) => {
+//   return new Promise ((resolve, reject ) => {
+//     if (param === 2) {
+//       resolve ("Halo, Ini kondisi true/berhasil")
+//     }else {
+//       reject("Halo, Ini kondisi false/gagal")
+//     }
+//   })
+// }
+
+// myFunctionPromise(4)
+// .then((result) => {
+//   console.log(result)
+// })
+// .catch((error) => {
+//   console.log(error)
+// })1
